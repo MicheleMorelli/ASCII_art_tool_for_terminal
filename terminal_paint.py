32ee34811b331959ec71cd4eaa51e5cda58161e7
MicheleMorelli <mmorel05@dcs.bbk.ac.uk>
2017-11-17 23:59:04 +0000

    added getch

--- terminal_paint.py
+++ terminal_paint.py
@@ -1,4 +1,54 @@
+#Getch class taken from http://code.activestate.com/recipes/134892/
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+getch = _Getch()
+#========================================================================
+
 def clean_screen():
     print("\n"*120)
 
+canvas = [[' ' for i in range(32)] for k in range(16)]
+
+for row in canvas:
+    print(*row, sep = '')
+
+cursor = (0,0)
+
+inp = getch()
+while inp != 'q':
+    print('you inputted',inp)
+    inp = getch()
 
