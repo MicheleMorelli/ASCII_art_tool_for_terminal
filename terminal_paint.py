2bee49b41dd5cf5b2661cda33638c577fbefa051
MicheleMorelli <mmorel05@dcs.bbk.ac.uk>
2017-11-18 00:58:28 +0000

    it works! :-)

--- terminal_paint.py
+++ terminal_paint.py
@@ -7,25 +7,29 @@ def draw_grid(grid):
     for row in grid:
         print(*row, sep = '')
 
+
 canvas = [[' ' for i in range(32)] for k in range(16)]
 cursor = [0,0]
-
-inp = getch()
+mode = 'write'
+inp = 'g'
 while inp != 'q':
     if inp == 'a':
         cursor[0] -= 1
     elif inp == 'd':
         cursor[0] += 1
     elif inp == 'w':
-        cursor[1] += 1
-    elif inp == 's':
         cursor[1] -= 1
-    x = cursor[0]
-    y = cursor[1]
-    for row in canvas:
-        for i in row:
-            i = ' '
-    canvas[x][y] = '#'
+    elif inp == 's':
+        cursor[1] += 1
+    elif inp == 'm':
+        if mode != 'write':
+            mode = 'write'
+        else:
+            mode = 'move'
+    x = cursor[1]
+    y = cursor[0]
+    if mode == 'write':
+        canvas[x][y] = '#'
     clean_screen()
     draw_grid(canvas)
     inp = getch()
