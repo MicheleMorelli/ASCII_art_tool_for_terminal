d13613b915bc694b5c573f082bdc7a4d5d10c5d2
MicheleMorelli <mmorel05@dcs.bbk.ac.uk>
2017-11-18 02:09:33 +0000

    basic functionality done

--- terminal_paint.py
+++ terminal_paint.py
@@ -9,42 +9,48 @@ def draw_grid(grid,width):
         print('|',end = '')
         print(*row,'|', sep = '')
     print('|' * (width + 2))
-
+    print('COMMANDS:\nm\tmove/write modes\nn\tdelete mode\nq\texit')
 
 width = 64
 height = 16
 
 canvas = [[' ' for i in range(width)] for k in range(height)]
-cursor = [0,0]
+x = 0
+y = 0
 mode = 'move'
 inp = 'g'
 prev = ' '
 while inp != 'q':
-    x = cursor[1]
-    y = cursor[0]
     if mode == 'move':
-        canvas[x][y] = prev
+        canvas[y][x] = prev
+    elif mode == 'delete':
+        canvas[y][x] = ' '
         
-    if inp == 'a':
-        cursor[0] -= 1
-    elif inp == 'd':
-        cursor[0] += 1
-    elif inp == 'w':
-        cursor[1] -= 1
-    elif inp == 's':
-        cursor[1] += 1
+    if inp == 'a' and x > 0:
+        x -= 1
+    elif inp == 'd' and x < width -1:
+        x += 1
+    elif inp == 'w' and y > 0:
+        y -= 1
+    elif inp == 's' and y < height - 1:
+        y += 1
     elif inp == 'm':
         if mode == 'write':
             mode = 'move'
         else:
             mode = 'write'
-    x = cursor[1]
-    y = cursor[0]
+    elif inp == 'n':
+        if mode != 'delete':
+            mode = 'delete'
+        else:
+            mode = 'move'
     if mode == 'write':
-        canvas[x][y] = '#'
+        canvas[y][x] = '#'
     elif mode == 'move':
-        prev = canvas[x][y]
-        canvas[x][y] = '@'
+        prev = canvas[y][x]
+        canvas[y][x] = '@'
+    elif mode == 'delete':
+        canvas[y][x] = '%'
     clean_screen()
     draw_grid(canvas, width)
     inp = getch()
