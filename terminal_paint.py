fcade1499b3e94476ab6d2d023f6ae9b16ed28bd
MicheleMorelli <mmorel05@dcs.bbk.ac.uk>
2017-11-18 01:38:35 +0000

    write and move modes ok

--- terminal_paint.py
+++ terminal_paint.py
@@ -3,20 +3,27 @@ from getch_class import *
 def clean_screen():
     print("\n"*120)
 
-def draw_grid(grid):
+def draw_grid(grid,width):
+    print('_' * (width+2))
     for row in grid:
-        print(*row, sep = '')
+        print('|',end = '')
+        print(*row,'|', sep = '')
+    print('|' * (width + 2))
 
 
-canvas = [[' ' for i in range(32)] for k in range(16)]
+width = 64
+height = 16
+
+canvas = [[' ' for i in range(width)] for k in range(height)]
 cursor = [0,0]
-mode = 'write'
+mode = 'move'
 inp = 'g'
+prev = ' '
 while inp != 'q':
     x = cursor[1]
     y = cursor[0]
     if mode == 'move':
-        canvas[x][y] = ' '
+        canvas[x][y] = prev
         
     if inp == 'a':
         cursor[0] -= 1
@@ -27,17 +34,18 @@ while inp != 'q':
     elif inp == 's':
         cursor[1] += 1
     elif inp == 'm':
-        if mode != 'write':
-            mode = 'write'
-        else:
+        if mode == 'write':
             mode = 'move'
+        else:
+            mode = 'write'
     x = cursor[1]
     y = cursor[0]
     if mode == 'write':
         canvas[x][y] = '#'
     elif mode == 'move':
+        prev = canvas[x][y]
         canvas[x][y] = '@'
     clean_screen()
-    draw_grid(canvas)
+    draw_grid(canvas, width)
     inp = getch()
 
