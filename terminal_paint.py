18b0477935082be1e55ad72628e5fa8060fde156
MicheleMorelli <mmorel05@dcs.bbk.ac.uk>
2017-11-18 00:42:45 +0000

    first functioning prototype

--- terminal_paint.py
+++ terminal_paint.py
@@ -7,20 +7,26 @@ def draw_grid(grid):
     for row in grid:
         print(*row, sep = '')
 
-
-
-
 canvas = [[' ' for i in range(32)] for k in range(16)]
-cursor = (0,0)
+cursor = [0,0]
 
 inp = getch()
 while inp != 'q':
     if inp == 'a':
+        cursor[0] -= 1
     elif inp == 'd':
+        cursor[0] += 1
     elif inp == 'w':
+        cursor[1] += 1
     elif inp == 's':
+        cursor[1] -= 1
+    x = cursor[0]
+    y = cursor[1]
+    for row in canvas:
+        for i in row:
+            i = ' '
+    canvas[x][y] = '#'
     clean_screen()
     draw_grid(canvas)
-    print('you inputted',inp)
     inp = getch()
 
