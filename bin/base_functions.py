e49d3fe0f1b0b035a2c3a795a658bdcb2e023947
MicheleMorelli <michele.morelli@london.ac.uk>
2018-02-16 11:35:24 +0000

    added some pydoc to base_functions

--- bin/base_functions.py
+++ bin/base_functions.py
@@ -1,7 +1,26 @@
+'''
+This module contains the base functions for the ASCII art tool.
+'''
+
 def clean_screen():
+    '''
+    Cleans the screen.
+    
+    Returns:
+        None
+    '''
     print("\n"*120)
 
 def draw_grid(grid,width, mode):
+    '''
+    Draws the canvas, based on the values of the 2d-list 'grid'.
+    Arguments:
+        grid aka the canvas aka 2d list 
+        width width of the canvas
+        mode current mode (e.g. "WRITE", or "MOVE")
+    Returns:
+        None
+    '''
     mode_str = mode + " MODE"
     number_of_spaces = width - len('COMMANDS:') - len(mode_str)
     print('_' * (width+2))
@@ -18,6 +37,15 @@ def draw_grid(grid,width, mode):
     print("COMMANDS:"+" " * number_of_spaces + mode_str + "\nm - move/write mode\th - clean screen\t[ and ] - change brush\nn - delete mode \to - export drawing\tj - paint bucket\nq - exit")
 
 def clean_grid(grid, height, width):
+    '''
+    Sets all the values of the canvas (aka the grid) to " ".
+    Arguments:
+        grid aka the canvas aka 2d list 
+        heigth heigth of the canvas
+        width width of the canvas
+    Returns:
+        None
+    '''
     for y in range(height):
         for x in range(width):
             grid[y][x] = ' ' 
@@ -39,3 +67,4 @@ def flood_fill(canvas,x, y, width, height, brushes, brush_cursor, previous_conte
     flood_fill(canvas,x-1, y, width, height, brushes, brush_cursor, previous_content)
     flood_fill(canvas,x, y+1, width, height, brushes, brush_cursor, previous_content)
     flood_fill(canvas,x, y-1, width, height, brushes, brush_cursor, previous_content)
+
